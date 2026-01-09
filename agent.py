ed676aaf14d0352f719116b2fd7c802f64376b3e
Yug Borana <yugborana000@gmail.com>
2026-01-09 13:36:02 +0530

    imp

--- agent.py
+++ agent.py
@@ -41,7 +41,7 @@ class ClinicAssistant(Agent):
         groq_model = openai.LLM(
             base_url="https://api.groq.com/openai/v1",
             api_key=os.environ.get("GROQ_API_KEY"),
-            model="meta-llama/llama-4-maverick-17b-128e-instruct - on_demand",
+            model="meta-llama/llama-4-scout-17b-16e-instruct",
         )
 
         today = datetime.datetime.now().strftime("%A, %B %d, %Y")
