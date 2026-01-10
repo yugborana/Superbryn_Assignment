fb0d610be71abc3c8971937d03e5e0bff2300dfc
Yug Borana <yugborana000@gmail.com>
2026-01-10 15:23:48 +0000

    imp

--- assistant.py
+++ assistant.py
@@ -23,7 +23,7 @@ class ClinicAssistant(Agent):
             groq_model = openai.LLM(
                 base_url="https://api.groq.com/openai/v1",
                 api_key=os.environ.get("GROQ_API_KEY"),
-                model="openai/gpt-oss-20b",
+                model="meta-llama/llama-4-scout-17b-16e-instruct",
             )
             
             # CARTESIA TTS SETUP:
